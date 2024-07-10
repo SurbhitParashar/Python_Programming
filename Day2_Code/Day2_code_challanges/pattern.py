b5962d25ab226e0d96c1fe7ffcf899ee8f2276fe
SurbhitParashar <140372413+SurbhitParashar@users.noreply.github.com>
2024-07-10 15:41:14 +0530

    Create pattern.py

---
+++ Day2_Code/Day2_code_challanges/pattern.py
@@ -0,0 +1,29 @@
+"""
+Code Challenge
+  Name: 
+    Pattern Builder
+  Filename: 
+    pattern.py
+  Problem Statement:
+    Write a Python program to construct the following pattern. 
+    Take input from User.  
+  Input: 
+    5
+  Output:
+    Below is the output of execution of this program.
+      * 
+      * * 
+      * * * 
+      * * * * 
+      * * * * * 
+      * * * * 
+      * * * 
+      * * 
+      * 
+"""
+
+num=int(input("enter a number"))
+for i in range(num):
+    print(i*"*")
+for i in range(num):
+    print((num-i)*"*")
