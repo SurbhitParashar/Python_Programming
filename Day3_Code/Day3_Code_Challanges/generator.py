f79d82a1114d6c1081d3bbfefc563d7bc8384dfb
SurbhitParashar <140372413+SurbhitParashar@users.noreply.github.com>
2024-07-22 18:31:29 +0530

    Create generator.py

---
+++ Day3_Code/Day3_Code_Challanges/generator.py
@@ -0,0 +1,20 @@
+"""
+Code Challenge
+  Name: 
+    generator
+  Filename: 
+    generator.py
+  Problem Statement:
+    This program accepts a sequence of comma separated numbers from user 
+    and generates a list and tuple with those numbers.  
+  Input: 
+    2, 4, 7, 8, 9, 12
+  Output:
+    List : ['2', ' 4', ' 7', ' 8', ' 9', '12'] 
+    Tuple : ('2', ' 4', ' 7', ' 8', ' 9', '122')
+"""
+
+inp_str=input("enter set of no.: ").split(',')
+# print(inp_str)
+print(list(inp_str))
+print(tuple(inp_str))
